8163ba83249502309a9a98db5dab6a790640c6dc
s1140304-glitch <s1140304@dmhs.kh.edu.tw>
2025-11-21 16:20:58 +0800

    hw6
    hw6

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"請輸入你的年齡:"
+    int age;
+    cin >> age;
+    if (age>= 18 ) {
+        cout << "18歲成年可以看 pxxxhub 網站" << end1;
+    }
+    if (age < 18) {
+        cout <<"未成年好好讀書" << end1;
+    }
+        
